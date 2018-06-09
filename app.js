dda72e27634a54a57a0bd4b61b7bb316ccdc1b4e
Pavan Kumar Gondhi <pavangondhi@gmail.com>
2018-06-09 16:49:12 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//index.js
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    router = express.Router(),
+    passport = require('passport'),
+    authController = require('./controllers/auth_controller'),
+    clientController = require('./controllers/client_controller'),
+    oauthController = require('./controllers/oauth_controller'),
+    userController = require('./controllers/users_controller'),
+    companyController = require('./controllers/company_controller'),
+    ip = require('ip');
+    schedule = require('node-schedule'),
+    moment = require('moment');
+
+var port = 8086;
+
+mongoose.connect('mongodb://localhost:27017/nodeAuth');
+
+//configure app
+{
+  app.set('views', __dirname + '/views')
+  app.set('view_options', {layout : false})
+
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: false }))
+  // parse application/json
+  app.use(bodyParser.json())
+
+  // the way to provide static contents
+  //app.use("/assets", express.static(__dirname + '/assets'))
+
+  app.use(function(req, res, next) { //allow cross origin requests
+        res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    });
+
+  // all the API's would be pre-fixed with /api/0.1 where 0.1 is its version number
+  app.use('/api/0.1', router);
+
+  // initialising the authentication middleware
+  app.use(passport.initialize());
+}
+
+//app.<REQUEST_METHOD>(<REQUEST_URI>, <CONTROLLER_METHOD>)
+// Signup and login requests
+router.post('/users/create', userController.createUser)
+router.post('/company/create', companyController.createCompany)
+
+
+//endpoint to get user details
+router.get('/users/:username',authController.isBearerAuthenticated, userController.getUser);
+
+
+app.listen(port);
+
+console.log("Base Server started at "  + port);
---
+++ controllers/company_controller.js
@@ -0,0 +1,143 @@
+var Company = require('../models/company_model');
+//users_controller.js
+var User = require("../models/users_model.js")
+var Token = require("../models/token_model.js")
+
+var Utils = require("../utils/utility.js")
+bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+exports.createCompany = function(req, res) {
+    var company = new Company(); // creating a new video object
+
+    // initialising the objects params got from the request body
+    company.name = req.body.companyName;
+    company.logo = req.body.companyLogo;
+    company.website = req.body.companyWebsite;
+    company.username = req.body.username;
+
+    // saving video on to the db
+    company.save(function(error) {
+      console.log("hello")
+        if (error) {
+            return res.json({ success : false, status : error }); // every json response will have these 2 fields
+        }
+        createUser(req, res);
+    });
+}
+
+createUser = function(req, res) {
+
+    // var EmailV, phoneV, field, value
+  	console.log("lai le")
+
+    // // check if email is sent or phone number is sent
+    // if (req.body.email) {
+    //   emailV = req.body.email
+    //   field = "email"
+    //   value = emailV
+    // }
+    // else
+    //   emailV = ""
+    //
+    // if (req.body.phone) {
+    //   phoneV = req.body.phone
+    //   field = "phone"
+    //   value = phoneV
+    // }
+    // else
+    //   phoneV = ""
+
+    var user = new User({
+    username: req.body.username,
+    // email:  emailV,
+    // phone: phoneV,
+    password: req.body.password,
+    company: req.body.companyName,
+    accountType: req.body.accountType,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    });
+
+    Utils.isUnique("username", req.body.username, function (err, result) {
+      if (err) return Utils.db_error(res);
+
+      if(!result) {
+
+      user.save(function(err) {
+
+      if (err) {
+      var duplicateKey = 11000
+      if (err.code == duplicateKey){
+        return Utils.send(res, "error", "user already exists")
+      }
+
+      return Utils.db_error(res,err)
+      }
+      else
+      Utils.send(res,"success", "user created successfully")
+
+      });
+
+      }
+      else {
+        Utils.send(res,"error", "email/phone already exists")
+      }
+
+  })
+
+}
+
+exports.loginUser = function(req, res) {
+
+  var query
+
+    if(req.body.email)
+    query = {'email':req.body.email}
+    else if (req.body.phone)
+    query = {'phone':req.body.phone}
+    else if (req.body.username)
+    query = {'username':req.body.username}
+
+    if(!query)
+      return Utils.send(res,"error", "required fields are empty")
+
+    User.findOne(query, function(err, user) {
+      if(err) return res.send(err);
+        if(user == null) {
+          Utils.send(res,"error", "invalid username/email/phone")
+
+        }
+        else {
+          auth(user)
+        }
+    });
+
+    function auth( user ) {
+      user.verifyPassword(req.body.password,function(err, isMatched) {
+        if(err) return res.send(err)
+        if (!isMatched) {
+        Utils.send(res,"error","invalid password")
+        }
+        else {
+          res.json({"username":user.username, "status":"success"})
+        }
+      });
+    }
+
+
+}
+
+// Function to get User details when username is passed
+exports.getUser = function(req,res) {
+  User.findOne({username:req.params.username} , function(err, user) {
+    if (err) return dbError(res, err)
+    // removing password field from user object
+    if(user)
+    {
+     user.password=undefined
+    }
+    res.json(user)
+  })
+}
