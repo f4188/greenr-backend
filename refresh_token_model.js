4511bed7b0d671bb13088b4f41b6121afe82b416
root <root@ip-172-31-31-185.us-east-2.compute.internal>
2018-06-10 00:18:36 +0000

    remove

--- refresh_token_model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var RefreshToken = new mongoose.Schema({
-    refresh_token : { type : String, required : true },
-    client_id : { type : String, required : true },
-    user_id :  { type :  String, required : true }
-});
-
-module.exports = mongoose.model('RToken', RefreshToken);
